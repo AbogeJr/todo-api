38ba287ca79b1b7fb60c2e902460da3b0165c1d0
Aloys Aboge Jr <76847396+AbogeJr@users.noreply.github.com>
2022-05-31 19:32:01 +0300

    added todos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const list = [
-  {
-      "id": 1,
-      "title": "Read The Alchemist",
-      "body": "A chapter a day keeps the nihilism away"
-  },
-  {
-      "id": 2,
-      "title": "Lmao",
-      "body": "I purpose to laugh my prosterior out"
-  },
-  {
-      "id": 3,
-      "title": "Dubs in Fo",
-      "body": "Steph Curry With the shot"
+
+class App extends Component {
+  state = {
+    todos: []
+  };
+
+  componentDidMount() {
+    this.getTodos();
   }
-]
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  getTodos() {
+    axios
+    .get('http://127.0.0.1:8000/api/')
+    .then(res => {
+    this.setState({ todos: res.data });
+    })
+    .catch(err => {
+    console.log(err);
+    });
+    }
+
+  render() {
+      return (
+        <div>
+          {this.state.todos.map(item => (
+          <div key={item.id}>
+            <h1>{item.title}</h1>
+            <span>{item.body}</span>
+          </div>
+          ))}
+        </div>
+        );
+}
 }
 
 export default App;
